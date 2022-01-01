72b59bf3db78afebf9385eb984119262d975703c
Max Kim <hwaseen@gmail.com>
2022-01-01 23:35:17 +0900

    Client: Change font loading method

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -10,8 +10,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
+            rel="preload"
             href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Gothic+A1:wght@500&display=swap"
-            rel="stylesheet"
           />
           <meta
             property="og:description"
