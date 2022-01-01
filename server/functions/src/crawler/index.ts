8fc38fd5c912b863b321bfe28cf9332595a928ea
Max Kim <hwaseen@gmail.com>
2022-01-01 14:29:03 +0900

    Server: Change return value to void

--- server/functions/src/crawler/index.ts
+++ server/functions/src/crawler/index.ts
@@ -41,18 +41,20 @@ const scrapAll = async (req: Request, res: Response) => {
     console.log(`DB에 ${tweets.length}개 트윗을 저장했습니다.`);
 
     console.log('크롤러를 종료하고 200 응답을 보냅니다.');
-    return res.status(200).send({
+    res.status(200).send({
       precedentLength: issueLength + recentLength,
       tweetLength: tweets.length,
     });
+    return;
   } catch (err: any) {
     console.log(
       `***에러가 발생했습니다. 크롤러를 종료하고 500 응답을 보냅니다.***`
     );
-    return res.status(500).send({
+    res.status(500).send({
       message: err.message,
       error: err,
     });
+    return;
   }
 };
 
@@ -104,8 +106,7 @@ const scrapRecent = async (req: Request, res: Response) => {
       );
       await dataBase.createTweets(tweets);
       console.log(`DB에 ${tweets.length}개 트윗을 저장했습니다.`);
-
-      return res.status(200).send({
+      res.status(200).send({
         precedentLength: newIssueLength + newRecentLength,
         tweetLength: tweets.length,
       });
@@ -113,18 +114,20 @@ const scrapRecent = async (req: Request, res: Response) => {
       console.log(
         `***새롭게 크롤링할 판례가 없습니다. 크롤러를 종료하고 200 응답을 보냅니다.***`
       );
-      return res.status(200).send({
+      res.status(200).send({
         message: '새롭게 크롤링할 판례가 없습니다!',
       });
     }
+    return;
   } catch (err: any) {
     console.log(
       `***에러가 발생했습니다. 크롤러를 종료하고 500 응답을 보냅니다.***`
     );
-    return res.status(500).send({
+    res.status(500).send({
       message: err.message,
       error: err,
     });
+    return;
   }
 };
 
